17aa26cc611fad95d869f42e8de462c20078bdef
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-06-24 09:59:36 +1000

    run build

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import cors from "cors";
 const app = express();
 
 const PORT = 8000;
+import path from "path";
+
+const __dirname = path.resolve();
+console.log(__dirname);
 
 // connect mongoDb
 
@@ -13,9 +17,15 @@ mongoConnect();
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.static(__dirname + "/build"));
+
 //api endpoints
 import taskRouter from "./src/routers/taskRouters.js";
 app.use("/api/v1/task", taskRouter);
+
+app.use("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.get("/", (req, res) => {
   res.json({
